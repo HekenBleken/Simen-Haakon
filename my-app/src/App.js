327c1014fbe194e710157bb9d492e879e0535f81
Håkon Thorstad Myhren <78857156+HekenBleken@users.noreply.github.com>
2023-04-24 19:41:46 +0200

    Navbar
    Created simple navbar for further development. Added css. H

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import './Navbar.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <p>
           <code>Koding er artig :-D</code>
